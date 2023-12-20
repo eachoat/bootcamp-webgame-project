1d7f2c7ed561006d10951d605b45476ed7196c7e
eachoat <eachoat@gmail.com>
2023-12-20 02:06:33 -0500

    12/20 Update 2

--- script.js
+++ script.js
@@ -1,59 +1,73 @@
-let currentQuestionIndex = 0;
-const questions_70s=[
-    {
-        Question:"Which Band wrote the song 'How Deep is Your Love'?",
-        Answers:[
+       let currentQuestionIndex = 0;
+        const questions_70s = [
             {
-                Answer:"The Beatles",
-                Correct: false,
-                
-                Answer:"Fleetwoord Mac",
-                Correct: false,
+                Question: "Which Band wrote the song 'How Deep is Your Love'?",
+                Answers: [
+                    {
+                        Answer: "The Beatles",
+                        Correct: false,
+                    },
+                    {
+                        Answer: "Fleetwood Mac",
+                        Correct: false,
+                    },
+                    {
+                        Answer: "Bee Gees",
+                        Correct: true,
+                    },
+                    {
+                        Answer: "Led Zeppelin",
+                        Correct: false,
+                    },
+                ],
+            },
+            
+        ];
 
-                Answer:"Bee Gees",
-                Correct: true,
-
-                Answer:"Led Zeppelin",
-                Correct: false,
-
+        function checkAnswer(isCorrect) {
+            if (isCorrect) {
+                alert("Correct!");
+            } else {
+                alert("Incorrect. Try Again");
             }
-        ]
+        }
+        function checkAndNext(isCorrect) {
+            
+
+            if (isCorrect) {
+
+            currentQuestionIndex++;
+
+            if (currentQuestionIndex < questions_70s.length) {
+                displayQuestion(currentQuestionIndex);
+            } else {
+                alert("Quiz completed!");
+            }
+        }
     }
-]
-function checkAnswer(isCorrect) {
-    if (isCorrect) {
-    alert("Correct!")
+        function displayQuestion(i) {
+            const decadeQuestions = document.getElementById(`decade-questions${i + 1}`);
+            const questionSetId = `70s-question-set-${i + 1}`;
+            decadeQuestions.innerHTML = document.getElementById(questionSetId).innerHTML;
+        }
 
-    
-    
-    currentQuestionIndex++
-    if (currentQuestionIndex < questions_70s.length) {
-        displayQuestion()
-    } else {
-        alert("Quiz completed!");
-    }
-}   else {
-    alert("Incorrect. Try Again")
-}}
+        document.addEventListener('DOMContentLoaded', function () {
+            const links = document.querySelectorAll('#home-screen ul a');
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    const links = document.querySelectorAll('#home-screen ul a')
-
-    links.forEach(function (link) {
-        link.addEventListener('click', function (event) {
-            event.preventDefault()
-            const decade = event.target.getAttribute('href').substring(1)
-            showDecade(decade)
+            links.forEach(function (link) {
+                link.addEventListener('click', function (event) {
+                    event.preventDefault();
+                    const decade = event.target.getAttribute('href').substring(1);
+                    showDecade(decade);
+                });
+            });
         });
-    });
-});
 
-function showDecade(decade) {
-    document.getElementById('home-screen').style.display = 'none'
-    const decadeQuestions = document.getElementById('decade-questions')
-    const questionSetId = `${decade}-question-set-1`
-    decadeQuestions.innerHTML = document.getElementById(questionSetId).innerHTML
+        function showDecade(decade) {
+            document.getElementById('home-screen').style.display = 'none';
+            const decadeQuestions = document.getElementById('decade-questions');
+            const questionSetId = `${decade}-question-set-1`;
+            decadeQuestions.innerHTML = document.getElementById(questionSetId).innerHTML;
 
-    decadeQuestions.style.display = 'block'
-}
+            decadeQuestions.style.display = 'block';
+        }
